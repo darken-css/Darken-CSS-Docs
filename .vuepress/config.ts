d6bbcfe0622afa699394bd49425ff9402af0dfb5
softcode <108250871+softcode589@users.noreply.github.com>
2022-10-15 14:22:33 -0600

    Deploy with VuePress now

---
+++ .vuepress/config.ts
@@ -0,0 +1,9 @@
+// .vuepress/config.ts
+import { defineUserConfig } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default defineUserConfig({
+  theme: hopeTheme({
+    // your theme config here
+  }),
+});
