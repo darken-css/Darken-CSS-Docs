7f12953269ac6968fe3e78fa48f7eb103fb9bb71
softcode <108250871+softcode589@users.noreply.github.com>
2022-10-15 15:36:03 -0600

    Revert back to Jekyll

--- .vuepress/config.ts
+++
@@ -1,9 +0,0 @@
-// .vuepress/config.ts
-import { defineUserConfig } from "vuepress";
-import { hopeTheme } from "vuepress-theme-hope";
-
-export default defineUserConfig({
-  theme: hopeTheme({
-    // your theme config here
-  }),
-});
